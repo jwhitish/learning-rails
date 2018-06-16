a66562c63a51a0775d9a7c313ed4edb5b8f8fc0d
jwhitish <jacobwhitish@gmail.com>
2018-06-16 15:55:17 -0700

    I3: number guessing game complete, incl cheat mode

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+$number = rand(100)
+
 get '/' do
-  number = rand(101)
-  erb:index, :locals => {:number => number}
+  message = check_guess(params["guess"])
+  erb :index, :locals => {:number => $number, :message => message}
+end
+
+def check_guess(guess)
+  if guess.nil?
+    return "Pick a number 1-100:"
+  else
+    if guess == "cheater"
+      message = "The number is #{$number}"
+    elsif guess.to_i > $number + 5
+      message = "Way too high!"
+    elsif guess.to_i < $number  - 5
+      message = "Way too low!"
+    elsif guess.to_i > $number
+      message = "Too high!"
+    elsif guess.to_i < $number
+      message = "Too low!"
+    else
+      message = "Correct! The secret number was #{$number}."
+    end
+  end
 end
