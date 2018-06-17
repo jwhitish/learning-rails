a3c8deb8d583b9bc60ee72bbe02d80145ad8d848
jwhitish <jacobwhitish@gmail.com>
2018-06-17 16:20:43 -0700

    web_guesser complete with extras

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -3,11 +3,19 @@ require 'sinatra/reloader'
 
 $number = rand(100)
 $background = 'white'
-$guesses = 5
+$remaining_guesses = 5
 
 get '/' do
-  message = check_guess(params["guess"])
-  erb :index, :locals => {:number => $number, :message => message, :background => $background, :guesses => $guesses}
+
+  if $remaining_guesses == 1
+    message = "Out of guesses! Number reset."
+    $remaining_guesses = 5
+    $number = rand(100)
+  else
+    message = check_guess(params["guess"])
+  end
+
+  erb :index, :locals => {:number => $number, :message => message, :background => $background, :remaining_guesses => $remaining_guesses}
 end
 
 def check_guess(guess)
@@ -15,36 +23,27 @@ def check_guess(guess)
     return
   else
     if guess == "cheater"
+      $remaining_guesses -= 1
       message = "The number is #{$number}"
     elsif guess.to_i > $number + 5
-      check_turn
+      $remaining_guesses -= 1
       $background = '#FF0000'
       message = "Way too high!"
     elsif guess.to_i < $number  - 5
-      check_turn
+      $remaining_guesses -= 1
       $background = '#0000FF'
       message = "Way too low!"
     elsif guess.to_i > $number
-      check_turn
+      $remaining_guesses -= 1
       $background = '#FF9473'
       message = "Too high!"
     elsif guess.to_i < $number
-      check_turn
+      $remaining_guesses -= 1
       $background = '#8888f8'
       message = "Too low!"
     else
       $background = '#008000'
       message = "Correct! The secret number was #{$number}."
     end
-  end
-end
-
-def check_turn
-  if $guesses == 0
-    $number = rand(100)
-    $guesses = 5
-    message = "Out of guesses! Secret number reset."
-  else
-    $guesses -= 1
   end
 end
