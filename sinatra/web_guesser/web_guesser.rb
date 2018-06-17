094fffe6ca75455b0b2d793c6e07e0b0673b022b
jwhitish <jacobwhitish@gmail.com>
2018-06-17 10:30:17 -0700

    added color display and css styling to web_guesser

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -2,28 +2,49 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 $number = rand(100)
+$background = 'white'
+$guesses = 5
 
 get '/' do
   message = check_guess(params["guess"])
-  erb :index, :locals => {:number => $number, :message => message}
+  erb :index, :locals => {:number => $number, :message => message, :background => $background, :guesses => $guesses}
 end
 
 def check_guess(guess)
   if guess.nil?
-    return "Pick a number 1-100:"
+    return
   else
     if guess == "cheater"
       message = "The number is #{$number}"
     elsif guess.to_i > $number + 5
+      check_turn
+      $background = '#FF0000'
       message = "Way too high!"
     elsif guess.to_i < $number  - 5
+      check_turn
+      $background = '#0000FF'
       message = "Way too low!"
     elsif guess.to_i > $number
+      check_turn
+      $background = '#FF9473'
       message = "Too high!"
     elsif guess.to_i < $number
+      check_turn
+      $background = '#8888f8'
       message = "Too low!"
     else
+      $background = '#008000'
       message = "Correct! The secret number was #{$number}."
     end
+  end
+end
+
+def check_turn
+  if $guesses == 0
+    $number = rand(100)
+    $guesses = 5
+    message = "Out of guesses! Secret number reset."
+  else
+    $guesses -= 1
   end
 end
