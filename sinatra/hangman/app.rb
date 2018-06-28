c4c95bab58a096393a1918982da26b70a3730a3a
jwhitish <jacobwhitish@gmail.com>
2018-06-28 07:07:20 -0700

    working out sessions. setting baseline

--- sinatra/hangman/app.rb
+++ sinatra/hangman/app.rb
@@ -3,27 +3,28 @@ require "sinatra/reloader"
 require_relative "lib/hangman.rb"
 
 enable :sessions
-
+@new_game = Hangman.new
 
 get "/" do
-  if params['button'] == 'New Game'
-    #session[:id].clear
-    #set new session
-    @disp_message = 'New game, begin!'
-  end
-
-  if session[:id] == nil
-    #start a new game
-    @disp_message = "ID nil"
+  if session[:hangman] == nil
+    session[:hangman] = Hangman.new
+    @disp_message = "'hangman' nil, new game!"
+    #@disp_message = session[:message]
   else
-    #session exists
+    #turn
   end
 
   erb :index, :locals => {:disp_message => @disp_message, :guesses => @guesses, :already_guessed => @already_guessed, :word => @word, :game_board => @game_board}
 end #end of get '/'
 
 post "/" do
-  @disp_message = "You guessed #{params['text']}"
+  if params["button"] == 'New Game'
+    session[:hangman] = @new_game
+    session[:message] = 'New game, begin!'
+  end
 
-  erb :index
+  session[:message] = "You guessed #{params["text"]}"
+
+
+  redirect '/'
 end
