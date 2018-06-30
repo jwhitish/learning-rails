0e18e87461a71334f4f2741f8e4fbd854971ec87
jwhitish <jacobwhitish@gmail.com>
2018-06-30 10:48:39 -0700

    new game and session reset working correctly

--- sinatra/hangman/app.rb
+++ sinatra/hangman/app.rb
@@ -3,28 +3,35 @@ require "sinatra/reloader"
 require_relative "lib/hangman.rb"
 
 enable :sessions
+set :session_secret, "super_secret"
 @new_game = Hangman.new
+
+before do #rbp
+  puts '[Params]'
+  p params
+end
 
 get "/" do
   if session[:id] == nil
     session[:id] = rand(100)
-    @disp_message = "'hangman' nil, new game!"
+    @disp_message = "'hangman' nil, new game!" #rbp
     set_state
   else
     #turn
-    @disp_message = "You guessed #{session[:guess]}"
+    @disp_message = "You guessed '#{session[:message]}'"
   end
 
   erb :index, :locals => {:disp_message => @disp_message, :guesses => @guesses, :already_guessed => @already_guessed, :word => @word, :game_board => @game_board}
 end #end of get '/'
 
 post "/" do
-  if params["button"] == 'New Game'
-    session[:hangman] = @new_game
-    session[:message] = 'New game, begin!'
+  if params["button"] == "New Game"
+    #session[:hangman] = @new_game
+    session.clear
   else
-    #   session[:hangman].playGame
-    session[:guess] = params["text"]
+    #session[:hangman].playGame
+    session[:guess] = params[:text]
+    session[:message] = session[:guess]
   end
   redirect '/'
 end #end post '/'
