238e196a500960844c46895333ee2a20985b9f7f
Jacob Whitish <jacobwhitish@gmail.com>
2018-08-02 06:57:09 -0700

    update api info

--- sinatra/portfolio/lib/ip_location.rb
+++ sinatra/portfolio/lib/ip_location.rb
@@ -13,7 +13,7 @@ def get_type(ip)
 end
 
 def get_city(ip)
-  city = HTTParty.get("http://api.ipstack.com/#{ip}?access_key=3817b6f234074b09022412c179ac185a&fields=city")
+  city = HTTParty.get("http://api.ipstack.com/#{ip}?access_key=#{ENV['IPSTACKKEY']}&fields=city")
   @cty = JSON.parse(city.body)
   @cty = @cty["city"]
 end
